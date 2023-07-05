f416e0c23028967ce18bd3bb2558bbe34ab92ae6
shivam <200020045@iitdh.ac.in>
2023-07-05 10:56:07 +0530

    file_with_serials

---
+++ scraping_v2.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from urllib.parse import urljoin
+import os
+
+urls = ['https://infocosevi.co.cr/']
+co = -1
+
+def scrape_links(curr_url, src_link):
+    
+    global co
+
+    if curr_url in src_link:
+        print('Yes, present')  # It works, further processing
+        response = requests.get(src_link)
+        js_content = response.text
+        co += 1
+        filename = f'{co}.js'
+
+        with open(filename, 'w') as f_1:
+            f_1.write(js_content)
+
+for url in urls:
+    try:
+        r = requests.get(url)
+        r.raise_for_status()  # Check for any HTTP request errors
+
+        html_content = r.content
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        with open('landing_page.html', 'w', encoding='utf-8') as f:
+            f.write(soup.prettify())
+
+        script_tags = soup.find_all('script')
+
+        with open('script_tags_file.js', 'a', encoding='utf-8') as f2:
+            for script_tag in script_tags:
+                script_content = script_tag.string
+
+                if script_content:
+                    f2.write(script_content + '\n')
+
+                else:
+                    tag = script_tag
+                    html = str(tag)
+                    soup1 = BeautifulSoup(html, 'html.parser')
+                    script_tag1 = soup1.find('script')
+                    src_link = script_tag1['src']
+                    print(src_link)
+
+                    scrape_links(url, src_link)  # Function call
+
+    except (requests.RequestException, IOError) as e:
+        print(f"Failed to fetch content for {url}: {e}")
+        continue
